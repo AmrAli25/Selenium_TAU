af2c93e9ed7e5d9e419ac9f10eaa574e9423a75f
Amr Ali <baianat@Amrs-iMAC.local>
2024-02-05 12:45:16 +0200

    Chapter 4.1 --> Page object module

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -3,18 +3,24 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import pages.HomePage;
 
 public class BaseTests {
     private WebDriver driver;
     protected HomePage homePage;
 
-
+    @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
         homePage = new HomePage(driver);
+    }
+
+    @AfterClass
+    public void tearDown(){
         driver.quit();
     }
     // This is the first example for Chapter 3
@@ -30,7 +36,4 @@ public class BaseTests {
 //        System.out.println("The items in the list is : " + links.size());
 //        driver.quit();
 //    }
-    public static void main(String[] args) {
-        BaseTests test = new BaseTests();
-    }
 }
